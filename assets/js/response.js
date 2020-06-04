5eb54912bb6f6db8c29b7205143b7f7dbee3f9eb
Manan Agrawal <44023521+mananag@users.noreply.github.com>
2020-06-04 16:32:49 +0530

    login and landing page edited

---
+++ assets/js/response.js
@@ -0,0 +1,11 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    console.log('working')
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav").style.top = "0";
+  } else {
+    document.getElementById("nav").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
